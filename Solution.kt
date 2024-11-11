fa4b140acc5f65fed3831d2c0ed46d9ebf51b00d
may4808 <mkik90911@naver.com>
2024-11-11 21:18:39 +0900

    프로그래머스 코딩테스트 Lv.1 :: 가장 많이 받은 선물

--- Solution.kt
+++ Solution.kt
@@ -34,3 +34,7 @@ class Solution {
 //2 ≤ callings의 길이 ≤ 1,000,000
 //callings는 players의 원소들로만 이루어져 있습니다.
 //경주 진행중 1등인 선수의 이름은 불리지 않습니다.
+
+//입출력 예
+//              players	                                          callings	                                          result
+//["mumu", "soe", "poe", "kai", "mine"]	               ["kai", "kai", "mine", "mine"]	              ["mumu", "kai", "mine", "soe", "poe"]
